3445494e6e2b308003f47cfba5cb822e3b06c80f
Leon T Chism II <leon@chism.org>
2014-05-27 17:05:45 -0500

    added some short circuiting to speed it up

--- eu_7.ex
+++ eu_7.ex
@@ -1,5 +1,7 @@
 defmodule Problem_7 do
 
+  # if we get all the way to trying the number itself. this shouldn't 
+  # happen with the 1/2 and 1/3 short circuits.
   def is_prime?(x, n) when x == n do
     true
   end
@@ -8,8 +10,26 @@ defmodule Problem_7 do
     false
   end
 
-  def is_prime?(x, n) do
+  # if we are checking a factor n where n > 3 and n > one third
+  # the number we are checking, then the number has to be prime
+  def is_prime?(x, n) when ((n > 3) and (n > ((x/3) + 1))) do
+    # IO.puts "special case for 3 for #{x}"
+    true
+  end
+
+  # if we are checking a factor n where n > 2 AND n > half 
+  # the number we are checking, then the number has to be prime
+  def is_prime?(x, n) when ((n > 2) and (n > ((x/2) + 1))) do
+    # IO.puts "special case for 2 for #{x}"
+    true
+  end
+
+  def is_prime?(x, n) when n == 2 do
     is_prime?(x,n+1)
+  end
+
+  def is_prime?(x, n) do
+    is_prime?(x,n+2)
   end
 
   def is_prime?(x) when x == 1 do
