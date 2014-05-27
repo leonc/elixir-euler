619c04f79fafad7e7a05d0b0cbc234ea2715af6c
Leon T Chism II <leon@chism.org>
2014-05-26 19:30:20 -0500

    added another solution that used a smarter approach

--- eu_6.ex
+++ eu_6.ex
@@ -7,3 +7,31 @@ defmodule Problem_6 do
   end
 
 end
+
+
+#TODO: i'm an idiot. there is a more clever solution. 
+# (1 + 2 + 3)(1 + 2 + 3) =
+# 1^2 + 1*2 + 1*3 + 1*2 + 2*2 + 2*3 + 3*1 + 3*2 + 3*3)
+# so the diff between that and 1^2 + 2^2 + 3^2 is 
+# 1*2 + 1*3 + 1*2 + 2*3 + 3*1 + 3*2
+# this is 2(every number times every other number)
+defmodule Problem_6b do
+  def clever(max) do
+    Enum.reduce(1..max, 0, fn(y,acc) -> acc + every_other(y,max) end)
+  end
+
+  def every_other(this_number, max) do
+    Enum.reduce(1..max, 0, fn(z, acc) -> acc + (
+      if (z == this_number) do
+        0
+      else
+       (this_number * z)
+      end
+      ) end )
+    # it would be awesome if this worked, but i think you have to add
+    # something to acc every time or it bonks.
+    # Enum.reduce(1..max, 0, fn(z, acc) -> unless (z == this_number), do: (acc + (this_number * z)) end ) 
+   end
+
+end
+
